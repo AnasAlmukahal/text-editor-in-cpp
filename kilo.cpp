523f14e4a899e5d2b696d82b76b5a1658f189077
AnasAlmukahal <new_email@example.com>
2024-06-20 18:13:41 +0300

    drawing tildes in each row, we'll do 24 rows since we dont know the size yet, and global state to know how many rows to draw and the size of the window using sys/ioctl.h

--- kilo.cpp
+++ kilo.cpp
@@ -4,8 +4,9 @@
 #include <unistd.h>
 #include <termios.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 using namespace std;
-#define CTRL_KEY(k) ((k)&0x1f)
+#define CTRL_KEY(k) ((k)& 0x1f)
 struct termios orig_termios;
 void killswitch(const char *s){
 	write(STDOUT_FILENO,"\x1b[2j",4);
@@ -13,13 +14,19 @@ void killswitch(const char *s){
 	perror(s);
 	exit(1);
 }
+struct editorConfig{
+	struct termios orig_termios;
+};
+struct editorConfig E;
 void RawmodeDisable(){
-	if(tcsetattr(STDIN_FILENO,TCSAFLUSH, &orig_termios)==-1)killswitch("tcgetattr");
+	if(tcsetattr(STDIN_FILENO,TCSAFLUSH, &E.orig_termios)==-1)
+		killswitch("tcgetattr");
 }
 void RawmodeEnable(){
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios)==-1)
+		killswitch("tcgetattr");
 	atexit(RawmodeDisable);
-	struct termios raw=orig_termios;
+	struct termios raw=E.orig_termios;
 	raw.c_iflag&=~(BRKINT|IXON|ICRNL|INPCK|ISTRIP);
 	raw.c_oflag&=~(OPOST);
 	raw.c_cflag|=(CS8);
@@ -36,6 +43,16 @@ char editorReadKey(){
 	}
 	return c;
 }
+int getWindowSize(int *rows,int*cols){
+	struct winsize ws;
+	if(ioctl(STDOUT_FILENO,TIOCGWINSZ, &ws)==-1||ws.ws_col==0)
+		return -1;
+	else{
+		*cols=ws.ws_col;
+		*rows=ws.ws_row;
+		return 0;
+	}
+}
 void editorProcessKeypress(){
 	char c=editorReadKey();
 	switch(c){
@@ -46,9 +63,15 @@ void editorProcessKeypress(){
 			break;
 	}
 }
+void editorDrawRows(){
+	for(int i=0;i<24;i++)
+		write(STDOUT_FILENO,"~/r/n",3);
+}
 void editorRefreshScreen(){
 	write(STDOUT_FILENO,"\x1b[2J",4);
 	write(STDOUT_FILENO, "\x1b[H",3);
+	editorDrawRows();
+	write(STDOUT_FILENO,"x1b[H",3);
 }
 int main()
 {
