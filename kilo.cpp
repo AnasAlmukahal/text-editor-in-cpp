7e60778b0901bdc59fc5356938ead9eda32c82b5
AnasAlmukahal <new_email@example.com>
2024-06-20 20:25:26 +0300

    flicker effect issue with the cursor displaying in the middle of the screen, and a way to clear lines one at a time

--- kilo.cpp
+++ kilo.cpp
@@ -109,6 +109,7 @@ void editorDrawRows(struct abuf *ab) {
 	  int y;
 	    for (y = 0; y < E.screenrows; y++) {
 		        abAppend(ab,"~",1);
+			abAppend(ab,"\x1b[k",3);
 			if(y<E.screenrows -1){
 				abAppend(ab,"\r\n",2);
 			}
@@ -118,11 +119,12 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
 	  struct abuf ab=ABUF_INIT;
-	  abAppend(&ab,"\x1b[2J",4);
+	  abAppend(&ab,"\x1b[?25l",6);
 	  abAppend(&ab,"\x1b[H",3);
 
 	      editorDrawRows(&ab);
 	      abAppend(&ab,"\x1b[H",3);
+	      abAppend(&ab,"\x1b[?25h",6);
 	        write(STDOUT_FILENO, ab.b, ab.len);
 		abFree(&ab);
 }
