b6e55ab92afa85b1b8635cd091d652f846b0c2c0
AnasAlmukahal <new_email@example.com>
2024-06-30 19:23:47 +0300

    incremental search done.

--- kilo.cpp
+++ kilo.cpp
@@ -68,7 +68,7 @@ editorConfig E;
 /** prototype**/
 void editorStatusMessage(const char*fmt,...);
 void editorRefreshScreen();
-char *editorPrompt(char* prompt, void(*callback)(char*,int));
+char *editorPrompt(const char* prompt, void(*callback)(char*,int));
 /*** terminal ***/
 
 void killswitch(const char *s) {
@@ -182,6 +182,18 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** row operations ***/
+int editorRowRxToCx(erow *row, int rx){
+  int cur_rx=0;
+  int cx;
+  for(cx=0;cx<row->size;cx++){
+    if(row->chars[cx]=='\t')
+    cur_rx+=(KILO_TAB_STOP-1)-(cur_rx%KILO_TAB_STOP);
+  cur_rx++;
+  if(cur_rx>rx)return cx;
+
+  }
+  return cx;
+}
 int editorRowCxtoRx(erow*row,int cx){
   int rx=0;
   for(int i=0;i<cx;i++){
@@ -327,7 +339,7 @@ void editorOpen(const char *filename) {
 }
 void editorSave(){
   if(E.filename==NULL)
-  E.filename=editorPrompt("save as: %s (ESC to cancel)");
+  E.filename=editorPrompt("save as: %s (ESC to cancel)",NULL);
   if(E.filename==NULL){
     editorStatusMessage("save aborted");
     return;
@@ -353,20 +365,53 @@ void editorSave(){
   editorStatusMessage("can't save! I/O error: %s",strerror(errno));
 }
 /** find **/
-void editorFind(){
-  char *query = editorPrompt("Search: &s (ESC to Cancel.)");
-  if(query==NULL)return;//if nothing is searched, do nothing.
+void editorFindCallBack(char* query, int key){
+  static int last_match=-1;
+  static int direction=1;
+  if(key=='\r'|| key=='\x1b'){
+    last_match=-1;
+    direction=1;
+    return;
+  }
+  else if(key==ARROW_RIGHT||key==ARROW_DOWN){
+    direction=1;
+  }else if(key==ARROW_LEFT||key==ARROW_UP){
+    direction=-1;
+  }else{
+    last_match=-1;
+    direction=1;
+  }
+  if(last_match==-1)direction=1;
+  int current=last_match;
   for(int i=0;i<E.numrows;i++){
-    erow *row=&E.row[i];
+    current+=direction;
+    if(current==-1)current=E.numrows-1;
+    else if(current==E.numrows)current=0;
+    erow *row=&E.row[current];
     const char *match=strstr(row->render.c_str(),query);
     if(match){
-      E.cy=i;
-      E.cx=editorRowRxToCx(row,match-row->render);
+      last_match=current;
+      E.cy=current;
+      E.cx=editorRowRxToCx(row,match-row->render.c_str());
       E.rowoff=E.numrows;
       break;
     }
   }
-  free(query);
+}
+void editorFind(){
+  int saved_cx=E.cx;
+  int saved_cy=E.cy;
+  int saved_coloff=E.coloff;
+  int saved_rowoff=E.rowoff;
+
+  char *query = editorPrompt("Search: &s (use ESC/Arrows/Enter)",editorFindCallBack);
+  if(query){free(query);}
+  else{
+    E.cx=saved_cx;
+    E.cy=saved_cy;
+    E.coloff=saved_coloff;
+    E.rowoff=saved_rowoff;
+  }
 }
 /*** append buffer ***/
 
@@ -419,18 +464,6 @@ void editorScroll() { // to set the E.rowoff value
   if (E.rx >= E.coloff + E.screencols) {
     E.coloff = E.rx - E.screencols + 1;
   }
-}
-int editorRowRxToCx(erow *row, int rx){
-  int cur_rx=0;
-  int cx;
-  for(cx=0;cx<row->size;cx++){
-    if(row->chars[cx]=='\t')
-    cur_rx+=(KILO_TAB_STOP-1)-(cur_rx%KILO_TAB_STOP);
-  cur_rx++;
-  if(cur_rx>rx)return cx;
-
-  }
-  return cx;
 }
 void editorDrawRows(abuf *ab) {
   int y;
@@ -523,7 +556,7 @@ void editorStatusMessage(const char* fmt,...){
 }
 
 /*** input ***/
-char* editorPrompt(char*prompt,void(*callback),(char*,int)) {
+char* editorPrompt(const char*prompt,void(*callback)(char*,int)) {
     size_t bufsize = 128;
     char* buf= (char*)malloc(bufsize); // Initialize buf with 128 characters of '\0'
     size_t buflen = 0;
@@ -537,12 +570,12 @@ char* editorPrompt(char*prompt,void(*callback),(char*,int)) {
           if(buflen!=0)buf[--buflen]='\0';
         }else if (c == '\x1b') {
             editorStatusMessage("");
-            if(callback)callback(buf,c);
+            if(callback) callback(buf,c);
             free(buf);
         } else if (c == '\r') {
             if (buflen != 0) {
                 editorStatusMessage("");
-                if(callback)callback(buf,c);
+                if(callback) callback(buf,c);
                 return buf;
             }
         } else if (!iscntrl(c) && c < 128) {
