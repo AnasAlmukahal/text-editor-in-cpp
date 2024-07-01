01f1b12a2aff6257737d78b98c21703222c1ca0f
AnasAlmukahal <new_email@example.com>
2024-07-01 23:20:26 +0300

    refactoring the coloring of numbers for future references

--- kilo.cpp
+++ kilo.cpp
@@ -38,6 +38,11 @@ enum editorKey {
   PAGE_UP,
   PAGE_DOWN
 };
+enum editorHighlight{
+  HL_NORMAL=0,//every other value
+  HL_NUMBER//every character that's part of a number will have that
+
+};
 
 /*** data ***/
 
@@ -46,6 +51,7 @@ struct erow {
   int rsize; // render size
   std::string chars;
   std::string render;
+  unsigned char * hl;
 };
 
 struct editorConfig {
@@ -180,7 +186,24 @@ int getWindowSize(int *rows, int *cols) {
     return 0;
   }
 }
+/** syntax highlighting **/
+void editorUpdateSyntax(erow *row){
+  row->hl=(unsigned char*)realloc(row->hl,row->rsize);//size of hl array= size of render array, so we use rsize for hl.
+  memset(row->hl,HL_NORMAL,row->rsize);//set all characters to HL_NORMAL by default before loop
+  int i;
+  for(i=0;i<row->rsize;i++){
+    if(isdigit(row->render[i])){
+      row->hl[i]=HL_NUMBER;
+    }
+  }
 
+}
+int editorSyntaxToColor(int hl){
+  switch(hl){
+    case HL_NUMBER: return 31;//31: foreground red
+    default: return 37;//37: foreground white
+  }
+}
 /*** row operations ***/
 int editorRowRxToCx(erow *row, int rx){
   int cur_rx=0;
@@ -224,6 +247,7 @@ void editorUpdateRow(erow *row) {
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+  editorUpdateSyntax(row);
 }
 void editorInsertRow(int at,const char *s, size_t len) {
   if(at<0 || at>E.numrows) return;
@@ -231,6 +255,7 @@ void editorInsertRow(int at,const char *s, size_t len) {
   E.row[at].size = len;
   E.row[at].chars = std::string(s, len);
   E.row[at].rsize = 0;
+  E.row[at].hl=NULL;
   editorUpdateRow(&E.row[at]);
   E.numrows++;
   E.dirty++;
@@ -238,6 +263,7 @@ void editorInsertRow(int at,const char *s, size_t len) {
 void editorFreeRow(erow *row){
   row->render.clear();
   row->chars.clear();
+  free(row->hl);
 }
 void editorDelrow(int at){
   if(at<0 || at>=E.numrows)return;//validate at
@@ -490,18 +516,29 @@ void editorDrawRows(abuf *ab) {
       if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
       char *c=&E.row[filerow].render[E.coloff];
+      unsigned char *hl=&E.row[filerow].hl[E.coloff];
+      int current_color=-1;
       int j;
       for(j=0;j<len;j++){
-        if(isdigit(c[j])){
-          abAppend(ab,"\x1b[31m",5);//31m is red
+        if(hl[j]==HL_NORMAL){
+          if(current_color!=-1){
+          abAppend(ab,"\x1b[39m",5);//39m:reset color
+          current_color=-1;
+          }
           abAppend(ab,&c[j],1);
-          abAppend(ab,"\x1b[39m",5);//39m is the reset color
         }else{
+          int color=editorSyntaxToColor(hl[j]);
+          if(color!=current_color){
+            current_color=color;
+          char buf[16];
+          int clen=snprintf(buf,sizeof(buf),"\x1b[%dm",color);
+          abAppend(ab,buf,clen);
+          }
           abAppend(ab,&c[j],1);
         }
       }
-    }
-
+          abAppend(ab,"\x1b[39m",5);//31m is red
+        }
     abAppend(ab, "\x1b[K", 3);
     if(y<E.screenrows-1)
       abAppend(ab, "\r\n", 2);
